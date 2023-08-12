4dae8fbb90cd452961ac4c5d70fbb4b560b792a6
laosijikaichele <laosijikaichele>
2023-08-13 05:21:25 +0800

    opt

--- microbench/src/main/java/io/netty/handler/codec/http/HttpStatusValueOfBenchmark.java
+++ microbench/src/main/java/io/netty/handler/codec/http/HttpStatusValueOfBenchmark.java
@@ -1,24 +1,17 @@
 package io.netty.handler.codec.http;
 import io.netty.microbench.util.AbstractMicrobenchmark;
-import io.netty.util.internal.SuppressJava6Requirement;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.TearDown;
-import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
-import io.netty.handler.codec.http.*;
 
 import java.util.Random;
-import java.util.SplittableRandom;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @BenchmarkMode(Mode.Throughput)
 @Warmup(iterations = 3, time = 1)
