23e8383b2d7ad3d011d23f2cf45c4dce8bf8ff2a
old driver <29225782+laosijikaichele@users.noreply.github.com>
2024-12-08 13:24:17 +0800

    Create AdaptiveByteBufAllocatorConcurrentNoCacheNormalThreadsCountBenchmark.java

---
+++ buffer/src/main/java/io/netty/buffer/AdaptiveByteBufAllocatorConcurrentNoCacheNormalThreadsCountBenchmark.java
@@ -0,0 +1,55 @@
+package io.netty.microbench.buffer;
+
+import io.netty.buffer.AdaptiveByteBufAllocator;
+import io.netty.buffer.ByteBuf;
+import io.netty.microbench.util.AbstractMicrobenchmark;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+@State(Scope.Benchmark)
+@Warmup(iterations = 5, time = 1)
+@Measurement(iterations = 5, time = 1)
+@Fork(1)
+public class AdaptiveByteBufAllocatorConcurrentNoCacheNormalThreadsCountBenchmark extends AbstractMicrobenchmark {
+
+    private static final AdaptiveByteBufAllocator adaptiveAllocator =
+            new AdaptiveByteBufAllocator(true, false);
+
+    @Param({
+            "123",
+            "1234",
+            "12345",
+            "123456",
+            "1234567",
+    })
+    public int size;
+
+    @Param({
+            "0",
+            "5",
+            "10",
+            "100",
+    })
+    public long tokens;
+
+    public AdaptiveByteBufAllocatorConcurrentNoCacheNormalThreadsCountBenchmark() {
+        super(false, true);
+    }
+
+    @Benchmark
+    @Threads(16)
+    public void allocateReleaseHeapAdaptive(Blackhole blackhole) {
+        ByteBuf buf = adaptiveAllocator.heapBuffer(size);
+        if (tokens > 0) {
+            Blackhole.consumeCPU(tokens);
+        }
+        blackhole.consume(buf.release());
+    }
+}
