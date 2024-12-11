135f820359435b46c991fd667c43dd30731d74f5
old driver <29225782+laosijikaichele@users.noreply.github.com>
2024-12-11 09:58:09 +0800

    Add files via upload

---
+++ buffer/src/main/java/io/netty/buffer/less-12345/BufAllocatorConcurrentCompareMultiLess12345Benchmark.java
@@ -0,0 +1,76 @@
+package io.netty.microbench.buffer;
+
+import io.netty.buffer.AdaptiveByteBufAllocator;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.microbench.util.AbstractMicrobenchmark;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
+
+@State(Scope.Benchmark)
+@Warmup(iterations = 1, time = 1)
+@Measurement(iterations = 1, time = 1)
+@Fork(1)
+public class BufAllocatorConcurrentCompareMultiLess12345Benchmark extends AbstractMicrobenchmark {
+
+    private final AdaptiveByteBufAllocator adaptiveAllocatorNoCache =
+            new AdaptiveByteBufAllocator(true, false);
+
+//    private final ByteBufAllocator defaultPooledAllocator = PooledByteBufAllocator.DEFAULT;
+
+//    private final AdaptiveByteBufAllocator adaptiveAllocatorCache =
+//            new AdaptiveByteBufAllocator(true, true);
+
+    @Param("123")
+    private int size;
+
+    private final String[] sizeParams = new String[13345 / 123];
+
+    public BufAllocatorConcurrentCompareMultiLess12345Benchmark() {
+        // Set the second param to 'false' when using 'adaptiveAllocatorCache'.
+        super(false, true);
+        sizeParams[0] = "123";
+        for (int i = 1; i < sizeParams.length; i++) {
+            sizeParams[i] = String.valueOf(Integer.parseInt(sizeParams[i-1]) + 123);
+        }
+    }
+
+    @Override
+    protected ChainedOptionsBuilder newOptionsBuilder() throws Exception {
+        return super.newOptionsBuilder().param("size", sizeParams);
+    }
+
+    @Benchmark
+    @Threads(8)
+    public void allocateReleaseHeap(Blackhole blackhole) {
+        ByteBuf buf = adaptiveAllocatorNoCache.heapBuffer(size);
+        blackhole.consume(buf);
+        blackhole.consume(buf.release());
+    }
+
+//    @Benchmark
+//    @Threads(8)
+//    public void allocateReleaseHeap(Blackhole blackhole) {
+//        ByteBuf buf = defaultPooledAllocator.heapBuffer(size);
+//        blackhole.consume(buf);
+//        blackhole.consume(buf.release());
+//    }
+
+//    @Benchmark
+//    @Threads(8)
+//    public void allocateReleaseHeap(Blackhole blackhole) {
+//        ByteBuf buf = adaptiveAllocatorCache.heapBuffer(size);
+//        blackhole.consume(buf);
+//        blackhole.consume(buf.release());
+//    }
+
+}
